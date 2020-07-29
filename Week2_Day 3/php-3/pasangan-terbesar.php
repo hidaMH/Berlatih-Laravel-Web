543727d16c5e91cd9f455dc16b266f53ff957c09
Hida Muhimmatul Husna <68956897+hidaMH@users.noreply.github.com>
2020-07-30 01:16:50 +0700

    Commit update

--- Week2_Day 3/php-3/pasangan-terbesar.php
+++ Week2_Day 3/php-3/pasangan-terbesar.php
@@ -1,6 +1,19 @@
 <?php
 function pasangan_terbesar($angka){
 // kode di sini
+  $arrAngka = str_split($angka, 1);
+    $temp = [];
+    $result = "";
+    for ($i = 0; $i < count($arrAngka) - 1; $i++) {
+        if ($i == 0) {
+            array_push($temp, (int)implode(array($arrAngka[$i], $arrAngka[$i + 1])));
+
+        } else if ($temp[0] < (int)implode(array($arrAngka[$i], $arrAngka[$i + 1]))) {
+            // array_replace($temp, array((int)implode(array($arrAngka[$i], $arrAngka[$i + 1]))));
+            $temp[0] = (int)implode(array($arrAngka[$i], $arrAngka[$i + 1]));
+        }
+    }
+    return $temp[0];
 }
 
 // TEST CASES
