eaca1eb5959b458dad1cf88196db9bf9e76f94c3
Hida Muhimmatul Husna <68956897+hidaMH@users.noreply.github.com>
2020-07-30 01:14:26 +0700

    Commit update

--- Week2_Day 3/php-3/palindrome-angka.php
+++ Week2_Day 3/php-3/palindrome-angka.php
@@ -2,6 +2,15 @@
 
 function palindrome_angka($angka) {
   // tulis kode di sini
+  if ($angka >= 0 && $angka < 9) {
+        return $angka+1;
+    } else {
+        for($i=$angka+1; $i<=1000000; $i++) {
+            if ($i == (int)strrev(implode(str_split(strval($i), 1)))) {
+                return $i;
+            } 
+        }
+    }
 }
 
 // TEST CASES
