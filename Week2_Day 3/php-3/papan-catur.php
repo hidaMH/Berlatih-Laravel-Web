0b473ad8e6a1d0d86603e7b09d42f5c39afa672d
Hida Muhimmatul Husna <68956897+hidaMH@users.noreply.github.com>
2020-07-30 01:15:48 +0700

    Commit update

--- Week2_Day 3/php-3/papan-catur.php
+++ Week2_Day 3/php-3/papan-catur.php
@@ -2,6 +2,15 @@
 
 function papan_catur($angka) {
 // tulis kode di sini
+ $catur = "";
+    for ($i = 0; $i < $angka; $i++) {
+        if ($i%2 == 0 ) {
+            $catur .= "".str_repeat("# ", $angka) ."\n";
+        } else {
+            $catur .= str_repeat(" #", $angka-1)."\n";
+        }
+    }
+    return $catur;
 }
 
 // TEST CASES
