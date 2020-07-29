7e9912ccdbcbb793516015b577357bb60e7f6368
Hida Muhimmatul Husna <68956897+hidaMH@users.noreply.github.com>
2020-07-30 01:12:16 +0700

    Update cari-mean.php

--- Week2_Day 3/php-3/cari-mean.php
+++ Week2_Day 3/php-3/cari-mean.php
@@ -2,9 +2,12 @@
 
 function cari_mean($arr){
 //kode di sini
+  $jumlah = array_sum($arr);
+  $panjang = count($arr);
+  return round($jumlah/$panjang) . "<br>";
 }
 
-TEST CASE 
+//TEST CASE 
 echo cari_mean([1, 2, 3, 4, 5]); // 3
 echo cari_mean([3, 5, 7, 5, 3]); // 5
 echo cari_mean([6, 5, 4, 7, 3]); // 5
